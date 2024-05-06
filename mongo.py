948c347a2c73cc611682f3d4b3d01b40f27ce61f
Sheethal <sh33thal24@gmail.com>
2024-05-06 10:14:47 +0530

    added more functions

--- mongo.py
+++ mongo.py
@@ -1,8 +1,11 @@
 
 from pymongo import MongoClient
 import json
+import io
 from openai import OpenAI
+from pathlib import Path
 from dotenv import load_dotenv
+from io import BytesIO
 
 
 
@@ -50,22 +53,94 @@ def insert_mem_data(email, first_name, last_name, date, description):
         {"email": email, "first_name": first_name, "last_name": last_name},
         {"$push": {"mem_data": new_memory}}
     )
+    
+def update_person_data(email, first_name, last_name, person_index, update_fields):
+    update_query = {"$set": {}}
+    for field, value in update_fields.items():
+        update_query["$set"]["people_data." + str(person_index) + "." + field] = value
 
+    collection.update_one(
+        {"email": email, "first_name": first_name, "last_name": last_name},
+        update_query
+    )
+    
+def update_place_data(email, first_name, last_name, person_index, update_fields):
+    update_query = {"$set": {}}
+    for field, value in update_fields.items():
+        update_query["$set"]["place_data." + str(person_index) + "." + field] = value
+
+    collection.update_one(
+        {"email": email, "first_name": first_name, "last_name": last_name},
+        update_query
+    )
+    
+def update_mem_data(email, first_name, last_name, person_index, update_fields):
+    update_query = {"$set": {}}
+    for field, value in update_fields.items():
+        update_query["$set"]["mem_data." + str(person_index) + "." + field] = value
+
+    collection.update_one(
+        {"email": email, "first_name": first_name, "last_name": last_name},
+        update_query
+    )
+
+def delete_person(email, first_name, last_name, person_index):
+    collection.update_one(
+        {"email": email, "first_name": first_name, "last_name": last_name},
+        {"$pull": {"people_data": {"$eq": collection.find_one({"email": email, "first_name": first_name, "last_name": last_name})['people_data'][person_index]}}}
+    )
+    
+def delete_place(email, first_name, last_name, place_index):
+    collection.update_one(
+        {"email": email, "first_name": first_name, "last_name": last_name},
+        {"$pull": {"place_data": {"$eq": collection.find_one({"email": email, "first_name": first_name, "last_name": last_name})['place_data'][place_index]}}}
+    )
+
+def delete_memory(email, first_name, last_name, mem_index):
+    collection.update_one(
+        {"email": email, "first_name": first_name, "last_name": last_name},
+        {"$pull": {"mem_data": {"$eq": collection.find_one({"email": email, "first_name": first_name, "last_name": last_name})['mem_data'][mem_index]}}}
+    )
+
+
+
+    collection.update_one(
+        {"email": email, "first_name": first_name, "last_name": last_name},
+        {"$unset": {"people_data." + str(person_index) + "." + field: ""}}
+    )
+
+
+#insert_person_data("sh33thal24@gmail.com", "Sheethal", "Joshi Thomas", "Joshi Thomas", "Broski", "Broing", "is hardworking and loves cars. likes dark pine green. is very extroverted.")
 #insert_place_data("sh33thal24@gmail.com", "Sheethal", "Joshi Thomas","CUSAT SOE", "This is where I study for college. Btech CSE is my course.", "img_url")
+# Update person data
+'''
+update_person_data("sh33thal24@gmail.com", "Sheethal", "Joshi Thomas", 0, {
+    "description": "Updated description",
+    "relation": "Uncle",
+    "occupation": "Engineer"
+})
+'''
 
-def export_to_json(filename):
+# Delete a person
+delete_person("sh33thal24@gmail.com", "Sheethal", "Joshi Thomas", 3)
+
+# Delete specific field from a person's data
+#delete_person_field("sh33thal24@gmail.com", "Sheethal", "Joshi Thomas", 0, "description")
+
+def export_to_json():
     data = list(collection.find())
 
     for item in data:
         item['_id'] = str(item['_id'])
-    with open(filename, "w") as json_file:
-        json.dump(data, json_file, indent=4)  
+    
+    json_bytes = json.dumps(data).encode('utf-8')
+    return BytesIO(json_bytes)
 
 
-def create_file(filename):
+def create_file(file_contents):
     client = OpenAI()
     file3 = client.files.create(
-        file=open(filename, "rb"),
+        file=(json_filename, file_contents, "application/json"),
         purpose="assistants"
     )
     return file3
@@ -75,10 +150,11 @@ def create_vector_store_file(file_id):
     client = OpenAI()
     vector_store_files = client.beta.vector_stores.files.list(vector_store_id='vs_l7uuSAnirZgPKhfnSriaOoTu')
     for vector_store_file in vector_store_files:
-        client.beta.vector_stores.files.delete(
+        vector_store_file1 = client.beta.vector_stores.files.delete(
             vector_store_id='vs_l7uuSAnirZgPKhfnSriaOoTu',
             file_id=vector_store_file.id
         )
+        print(vector_store_file1)
     vector_store_file = client.beta.vector_stores.files.create(
         vector_store_id='vs_l7uuSAnirZgPKhfnSriaOoTu',
         file_id=file_id
@@ -86,10 +162,9 @@ def create_vector_store_file(file_id):
     return vector_store_file
 
 json_filename = "data.json"
-export_to_json(json_filename)
+json_file_contents = export_to_json()
 
-
-created_file = create_file(json_filename)
+created_file = create_file(json_file_contents)
 #print("File created:", created_file)
 
 
@@ -98,16 +173,11 @@ print("Vector store file created:", vector_store_file)
 
 client = OpenAI()
 
-
-
-
-
-
 vector_store_files = client.beta.vector_stores.files.list(
   vector_store_id='vs_l7uuSAnirZgPKhfnSriaOoTu'
 )
 print(vector_store_files)
 
-#insert_person_data("sh33thal24@gmail.com", "Sheethal", "Joshi Thomas", "Joshi Thomas", "Father", "Business", "is hardworking and loves cars. likes dark pine green. is very extroverted.")
+
 
 
